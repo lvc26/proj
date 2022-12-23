e55927343b8e3828ce6ebccaa673fd906e7159f5
Elena <116493650+lvc26@users.noreply.github.com>
2022-12-23 03:55:16 +0300

    Update forms.py

--- e-shop/shop/mainapp/forms.py
+++ e-shop/shop/mainapp/forms.py
@@ -6,12 +6,21 @@ from .models import Order
 class OrderForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
+        """
+        Функция __init__ вызывается при создании экземпляра класса.
+        Функция super() используется для предоставления доступа к методам и свойствам родительского или родственного
+        класс.
+        *args и **kwargs используются для передачи функции переменного числа аргументов.
+        Атрибут label используется для указания текста для метки.
+        """
         super().__init__(*args, **kwargs)
         self.fields['order_date'].label = 'Дата получения заказа'
 
+    # Установка типа поля ввода на дату.
     order_date = forms.DateField(widget=forms.TextInput(attrs={'type': 'date'}))
 
     class Meta:
+    # Определение полей, которые будут отображаться в форме.
         model = Order
         fields = (
             'first_name', 'last_name', 'phone', 'address', 'buying_type', 'order_date', 'comment'
