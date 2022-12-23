d4a985cbbc887160304622409978fd38fffcb37f
Elena <116493650+lvc26@users.noreply.github.com>
2022-12-23 03:57:07 +0300

    Update utils.py

--- e-shop/shop/mainapp/utils.py
+++ e-shop/shop/mainapp/utils.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 def recalc_cart(cart):
+    """
+    Фукция пересчитывает конечную цену и общее количество товаров в корзине.
+    :param cart: Объект в корзине.
+    """
     cart_data = cart.products.aggregate(models.Sum('final_price'), models.Count('id'))
     if cart_data.get('final_price__sum'):
         cart.final_price = cart_data['final_price__sum']
