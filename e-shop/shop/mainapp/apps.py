a105e3abf34f73bf6237c89948cd2c47dc4ed5e1
Elena <116493650+lvc26@users.noreply.github.com>
2022-12-23 03:54:38 +0300

    Update apps.py

--- e-shop/shop/mainapp/apps.py
+++ e-shop/shop/mainapp/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class MainappConfig(AppConfig):
+    # Создаем новое приложение под названием mainapp.
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'mainapp'
